2465292473878f344e829531a314a85d42610de3
InviseDivine <grisagorbacev65@gmail.com>
2026-05-05 21:14:17 +0200

    jopa

--- project/android/jni/Android.mk
+++ project/android/jni/Android.mk
@@ -109,6 +109,7 @@ LOCAL_SRC_FILES := ../../../src/main.cpp \
 ../../../src/client/renderer/Textures.cpp \
 ../../../src/client/renderer/TileRenderer.cpp \
 ../../../src/client/renderer/gles.cpp \
+../../../src/world/level/LevelConstants.cpp \
 ../../../src/client/renderer/culling/Frustum.cpp \
 ../../../src/client/renderer/entity/ArrowRenderer.cpp \
 ../../../src/client/renderer/entity/ChickenRenderer.cpp \
