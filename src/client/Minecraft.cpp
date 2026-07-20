9434a48ae75a71804a6a8a52ff202c2af1b572d1
Kolyah35 <kolyah35@sffempire.ru>
2026-07-20 23:10:21 +0300

    Revert "help me"
    This reverts commit 79be815827f1b0ae31e0961e5f74d677fcb80e97.

--- src/client/Minecraft.cpp
+++ src/client/Minecraft.cpp
@@ -1197,15 +1197,12 @@ void Minecraft::init()
 	TextureId grassId = (textures->loadTexture("misc/grasscolor.png")); // loading the uh png for foliage color
 	int* grassPixels = textures->loadTexturePixels(grassId, "misc/grasscolor.png");
 	GrassColor::init(grassPixels);
-#ifndef __APPLE__
+
 	bool tint = options.getBooleanValue(OPTIONS_FOLIAGE_TINT); // finally, toggleable foliage color
-	bool sideTint = options.getBooleanValue(OPTIONS_TINTED_SIDE);
-#else
-	bool tint = false, sideTint = false;
-#endif
 	FoliageColor::setUseTint(tint);
 	GrassColor::setUseTint(tint);
 
+	bool sideTint = options.getBooleanValue(OPTIONS_TINTED_SIDE);
 	TileRenderer::setUseTint(sideTint);
 
 
